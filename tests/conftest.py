8122d1f236bd47ac4fddb3a22c25fff46bf709db
Sebastian Gsell <sebastian.gsell93@gmail.com>
2024-03-13 13:22:27 +0100

    Cleanup

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,8 @@
-import glob
 import os
 import sys
 from pathlib import Path
 
 import jax
-import pandas as pd
-import pytest
-import yaml
 
 # Obtain the test directory of the package
 TEST_DIR = Path(__file__).parent
@@ -15,9 +11,6 @@ TEST_DIR = Path(__file__).parent
 REPLICATION_TEST_RESOURCES_DIR = TEST_DIR / "resources" / "replication_tests"
 
 WEALTH_GRID_POINTS = 100
-
-NUMBA_DISABLE_JIT = 1
-
 
 # Add the utils directory to the path so that we can import helper functions.
 sys.path.append(os.path.join(os.path.dirname(__file__), "utils"))
@@ -35,36 +28,3 @@ def pytest_configure(config):
 def pytest_unconfigure(config):
     """Called before test process is exited."""
     os.environ.pop("NUMBA_DISABLE_JIT", None)
-
-
-@pytest.hookimpl(tryfirst=True)
-def pytest_sessionfinish(session, exitstatus):  # noqa: ARG001
-    # Get the current working directory
-    cwd = os.getcwd()
-
-    # Search for .npy files that match the naming pattern
-    pattern = os.path.join(cwd, "[endog_grid_, policy_, value_]*.npy")
-    npy_files = glob.glob(pattern)
-
-    # Delete the matching .npy files
-    for file in npy_files:
-        os.remove(file)
-
-
-@pytest.fixture(scope="session")
-def load_example_model():
-    def load_options_and_params(model):
-        """Return parameters and options of an example model."""
-        params = pd.read_csv(
-            REPLICATION_TEST_RESOURCES_DIR / f"{model}" / "params.csv",
-            index_col=["category", "name"],
-        )
-        params = (
-            params.reset_index()[["name", "value"]].set_index("name")["value"].to_dict()
-        )
-        options = yaml.safe_load(
-            (REPLICATION_TEST_RESOURCES_DIR / f"{model}" / "options.yaml").read_text()
-        )
-        return params, options
-
-    return load_options_and_params
